af58690b4ce20c8ff1ee9b5c3e29b4a6517d76cc
Xinyi Yang <yang_0622@live.cn>
2022-05-19 17:21:44 -0700

    update oop version of coffee machine

--- OOP/main.py
+++
@@ -1,7 +0,0 @@
-from prettytable import PrettyTable
-table = PrettyTable()
-data = {"Pokemon Name": ["Pikachu", "Squirtle", "Charmander"], "Type": ["Electric", "Water", "Fire"]}
-for key in data:
-    table.add_column(key, data[key])
-table.align = "l"
-print(table)
--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,26 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+CoffeeMaker = CoffeeMaker()
+MoneyMachine = MoneyMachine()
+valid_input = ["espresso", "latte", "cappuccino", "report", "off"]
+
+menu = Menu()
+while True:
+    order_name = input(f"What would you like? {menu.get_items()}\t")
+    while order_name not in valid_input:
+        print("Invalid Input. Try again.")
+        order_name = input(f"What would you like? {menu.get_items()}\t")
+    if order_name == "off":
+        quit()
+    elif order_name == "report":
+        CoffeeMaker.report()
+        MoneyMachine.report()
+    else:
+        drink = menu.find_drink(order_name)
+        if drink:
+            if CoffeeMaker.is_resource_sufficient(drink) and MoneyMachine.make_payment(drink.cost):
+                CoffeeMaker.make_coffee(drink)
+
+
+
