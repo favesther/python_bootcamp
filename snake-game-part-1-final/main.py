b5a8be7eacce4b17aa3dbe173269c12b4cac752a
Xinyi Yang <yang_0622@live.cn>
2022-06-09 18:07:12 -0700

    snake game start

--- snake-game-part-1-final/main.py
+++ snake-game-part-1-final/main.py
@@ -1,28 +1,40 @@
-from turtle import Screen
+# from turtle import Screen
+from turtle import *
 from snake import Snake
 import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor("black")
+# screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-snake = Snake()
-food = Food()
-
-screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
-screen.onkey(snake.right, "Right")
-
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-
-    snake.move()
+body = []
+x, y = 0, 0
+for _ in range(2):
+    tim = Turtle()
+    # tim.color("white", "white")
+    # tim.shape("square")
+    x -= 10
+    tim.setposition(x, y)
+    body.append(tim)
 
 
+# snake = Snake()
+# food = Food()
+#
+# screen.listen()
+# screen.onkey(snake.up, "Up")
+# screen.onkey(snake.down, "Down")
+# screen.onkey(snake.left, "Left")
+# screen.onkey(snake.right, "Right")
+#
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
+#
+#     snake.move()
+#
+#
 screen.exitonclick()
