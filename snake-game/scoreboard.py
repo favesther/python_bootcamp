7fdcf51138b1438be47016f789503740bc2bb23b
Xinyi Yang  (Esther) <yang_0622@live.cn>
2022-07-19 15:06:04 -0700

    file read/write

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -7,8 +7,9 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open('data.txt') as data:
+            self.high_score = int(data.read())
         self.score = 0
-        self.high_score = 0
         self.hideturtle()
         self.penup()
         self.goto(0, 270)
@@ -26,6 +27,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.high_score < self.score:
             self.high_score = self.score
+            with open('data.txt', mode = 'w') as data:
+                data.write(f'{self.high_score}')
         self.score = 0
         self.update_scoreboard()
 
